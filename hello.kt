c9dd3bb3dae47a4261228f4a07c7ce39da3d6b40
Anna Gabain <anna.gabain@outlook.com>
2022-10-23 15:05:35 +0000

    test calling the functions inside main function

--- hello.kt
+++ hello.kt
@@ -1,13 +1,26 @@
 fun main() {
   println("Hello, World!")
+  println("-----------------")
   com()
+  ve()
 }
 
 //call com funciton inside the main funciton
+// Code prints "Howdy, partner!" to the terminal.
 fun com() {
   println("Howdy, partner!") 
 }
 
-// Code prints "Howdy, partner!" to the terminal.
+/* I have learned that Kotlin synthax is somehow similar to JavaScript*/
 
-/* I have learned that Kotlin synthax somehow similar to JavaScript*/
+fun ve() {
+  println("Down by the bay")
+  println("Where the watermelons grow,")
+  println("Back to my home")
+  println("I dare not go.")
+  println("For if I do")
+  println("My mother will say")
+  println("Did you ever use an app that taught you how to snap?")
+  println("Down by the bay!")
+}
+  
