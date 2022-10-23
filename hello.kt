94d90feb231bd5e35efdd1ea179756058fde4638
Anna Gabain <anna.gabain@outlook.com>
2022-10-23 15:35:58 +0200

    Update hello.kt

--- hello.kt
+++ hello.kt
@@ -1,3 +1,13 @@
 fun main() {
   println("Hello, World!")
 }
+
+
+fun com() {
+  println("Howdy, partner!") 
+}
+
+// Code prints "Howdy, partner!" to the terminal.
+
+/* I have learned that Kotlin is 
+very similar to JavaScript!*/
